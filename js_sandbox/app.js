66d825751280859b6dbdd3007ae9e687256b79ba
John <casillaj@oregonstate.edu>
2018-10-18 15:59:37 -0600

    Updated project to include some console notes

---
+++ js_sandbox/app.js
@@ -0,0 +1,29 @@
+// Log to console
+
+console.log('Hello World');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1,b:2});
+console.table({a:1,b:2});
+
+console.error('This is some error');
+console.clear(); // This clears the console.
+console.warn('This is a warning');
+
+// Times how long it takes to run between 'time' and 'timeEnd'
+console.time('Hello');
+
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+    console.log('Hello World');
+
+console.timeEnd('Hello');
