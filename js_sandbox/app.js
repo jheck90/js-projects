49c9ecde1e33276beba79b87c6c5dbef0dcb698d
John <casillaj@oregonstate.edu>
2018-10-22 16:33:31 -0600

    Adding IF statement stuff

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,26 +1,77 @@
-let val;
+const id = 100;
 
-const today = new Date();
-let birthday = new Date('9/10/1981 11:25:00');
-birthday = new Date('September 10 1981');
-birthday = new Date('9/10/1981');
+// // EQUAL TO
+// if(id == 100){
+//     console.log('CORRECT');
+// } else {
+//         console.log('INCORRECT');
+// }
 
+// // NOT EQUAL TO
+// if(id != 101){
+//     console.log('CORRECT');
+// } else {
+//         console.log('INCORRECT');
+// }
 
-// Months are 0 based, so Nov shows as '9'
-val = today.getMonth();
-val = today.getDate();
-// 0 based, starting on sunday as 0
-val = today.getDay();
-val = today.getFullYear();
-val = today.getHours();
-val = today.getMinutes();
-val = today.getSeconds();
-val = today.getMilliseconds();
-val = today.getTime();
+// // EQUAL TO VALUE AND TYPE
+// if(id === 100){
+//     console.log('CORRECT');
+// } else {
+//         console.log('INCORRECT');
+// }
 
-birthday.setMonth(2);
-birthday.setDate(12);
-birthday.setFullYear(1985);
-birthday.setHours(3);
+// // EQUAL TO VALUE AND TYPE
+// if(id !== 100){
+//     console.log('CORRECT');
+// } else {
+//         console.log('INCORRECT');
+// }
 
-console.log(birthday);
+// Test if undefined
+// if(typeof id !== 'undefined'){
+//     // gotta use back ticks to do this line below
+//     console.log(`The ID is ${id}`);
+// } else {
+//     console.log('NO ID');
+// }
+
+// IF ELSE
+// const color = 'yellow';
+
+// if(color === 'red'){
+//     console.log('color is red');
+// } else if(color === 'blue'){
+//     console.log('colow is blue');
+// } else {
+//     console.log('Color is not red or blue');
+// }
+
+// LOGICAL OPERATORS
+const name = 'Steve';
+const age = 4;
+
+// AND &&
+if(age > 0 && age < 12){
+    console.log(`${name} is a child`);
+} else if(age >= 13 && age <= 19){
+    console.log(`${name} is a teenager`);
+} else {
+    console.log(`${name} is an adult`);
+}
+
+// OR ||
+if(age < 16 || age > 65){
+    console.log(`${name} can not run in race`)
+} else {
+    console.log(`${name} is registered for race`)
+}
+
+// TERNARY OPERATOR
+console.log(id === 100 ? 'Correct' : 'INCORRECT');
+
+// WITHOUT BRACES
+if(id === 100)
+    console.log('CORRECT');
+else
+    console.log('INCORRECT');
