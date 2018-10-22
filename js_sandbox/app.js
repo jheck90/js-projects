3d58de914da4fe9efce8f39b102df2c0d12d818c
John <casillaj@oregonstate.edu>
2018-10-22 15:34:02 -0600

    Adding object literals, JS

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,65 +1,41 @@
-// Create some arrays, 2 ways to create array
-const numbers = [43,56,33,23,44,36,5];
-const numbers2 = new Array(22,45,33,76,54);
-const fruit = ['Apple','Banana', 'Orange', 'Pear']
-const mixed = [22, 'Hello', true, undefined, null, {a:1,b:1}, new Date()];
+const person = {
+    firstName: 'Steve',
+    lastName: 'Smith',
+    age: 30,
+    email: 'steve@aol.com',
+    hobbies: ['music', 'sports'],
+    address: {
+        city: 'Miami',
+        state: 'Florida'
+    },
+    getBirthYear: function(){
+        // since we're an object within object, we have to use 'this'
+        return 2018 - this.age;
+    }
+}
 
 let val;
 
-// Get array length
-val = numbers.length;
-// Check to see if it is array
-val = Array.isArray(numbers);
-
-// Get single value
-val = numbers[3];
-val = numbers[0];
-
-// Insert into array
-numbers[2] = 100;
-
-// Find index of value
-val = numbers.indexOf(36);
-
-// MUTATING ARRAYS
-// numbers.push(250);      // adds number onto end of array
-
-// numbers.unshift(120);   // Adds number to front
-
-// numbers.pop();          // Removes number from end (250 from above)
-
-// numbers.shift();        // Removes from front
-
-// numbers.splice(1,1);    // Where we want to start and end, remove that
-
-// numbers.reverse();      // Reverses array
-
-// Concatenate Arrays
-val = numbers.concat(numbers2);
-
-// Sort arrays
-val = fruit.sort();
-// val = numbers.sort();
-
-// // Use the "compare function" to sort
-// val = numbers.sort(function(x, y){
-//     return x - y;
-// });
-
-// // Reverse sort
-// val = numbers.sort(function(x, y){
-//     return y - x;
-// });
-
-// Find
-function under50(num){
-    return num < 50;
-}
-
-// Uses the above function to find first number in array under 50
-val = numbers.find(under50);
+val = person;
+// Get Specific value
+val = person.firstName;
+val = person['firstName'];
+val = person.age;
+val = person.hobbies;
+val = person.address.state;
+val = person.address['city'];
+val = person.getBirthYear();
 
 
-
-console.log(numbers);
 console.log(val);
+
+// array of objects
+const people = [
+    {name: 'john', age: 30},
+    {name: 'mike', age: 23}
+];
+
+//a loop to show all people in that array above
+for (let i = 0; i < people.length; i++){
+    console.log(people[i].name);
+}
