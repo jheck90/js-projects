9956d289ce24e4ad48f7d3030270434f93b0c684
John <casillaj@oregonstate.edu>
2018-10-23 11:55:16 -0600

    updated html and app.js to include DOM stuff

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,63 +1,41 @@
-// WINDOW METHODS / OBJECTS / PROPERTIES
-
-// Alert
-//alert('Hello World!')
-
-// Prompt
-// const input = prompt();
-// alert(input);
-
-// Confirm
-// if(confirm('Are you sure?')){
-//     console.log('Yes');
-// } else {
-//     console.log('NO');
-// }
-
 let val;
 
-// Outer height and width
-val = window.outerHeight;
-val = window.outerWidth;
-
-// Inner height and width (not scroll bars and ish)
-val = window.innerHeight;
-val = window.innerWidth;
-
-// Scroll points (figuring out where you in terms of scrolling)
-val = window.scrollY;
-val = window.scrollX;
-
-// Location Object
-val = window.location;
-val = window.location.hostname;
-val = window.location.port;
-val = window.location.href;
-val = window.location.search;
-
-// Redirect
-// window.location.href = 'http://google.com';
-
-// Reload
-// window.location.reload();
-
-// History Object
-// window.history.go(-5);
-
-// val = window.history.length
-
-// Navigator Object
-val = window.navigator;
-val = window.navigator.appName;
-val = window.navigator.appVersion;
-val = window.navigator.userAgent;
-val = window.navigator.platform;
-val = window.navigator.vendor;
-val = window.navigator.language;
+// Entire HTML document
+val = document;
+val = document.all;
+val = document.all[10];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
 
 
+val = document.forms;
+val = document.forms[0];
+val = document.forms[0].id;
+val = document.forms[0].method;
+
+val = document.links;
+
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList[0];
+
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[2].getAttribute('src');
 
 
+let scripts = document.scripts;
+let scriptsArr = Array.from(scripts);
+scriptsArr.forEach(function(script){
+    console.log(script);
+});
 
 console.log(val);
 
