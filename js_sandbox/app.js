c2a96ced6fc534c186e9c2ccc683003c6abac202
John <casillaj@oregonstate.edu>
2018-10-23 12:40:28 -0600

    Adding sweet single elem manip stuff

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,41 +1,36 @@
-let val;
+// document.getElementById()
 
-// Entire HTML document
-val = document;
-val = document.all;
-val = document.all[10];
-val = document.all.length;
-val = document.head;
-val = document.body;
-val = document.doctype;
-val = document.domain;
-val = document.URL;
+console.log(document.getElementById('task-title'));
 
+// Get things from the element
+console.log(document.getElementById('task-title').id);
+console.log(document.getElementById('task-title').className);
 
-val = document.forms;
-val = document.forms[0];
-val = document.forms[0].id;
-val = document.forms[0].method;
+const taskTitle = document.getElementById('task-title');
 
-val = document.links;
+// Change Styling
+taskTitle.style.background = '#333';
+document.getElementById('task-title').style.color = '#fff';
+document.getElementById('task-title').style.padding = '5px';
+// document.getElementById('task-title').style.display = 'none'; // makes disappear
 
-val = document.links;
-val = document.links[0];
-val = document.links[0].id;
-val = document.links[0].className;
-val = document.links[0].classList[0];
-
-val = document.images;
-
-val = document.scripts;
-val = document.scripts[2].getAttribute('src');
+// Change Content
+taskTitle.textContent = 'Tasks List'
+taskTitle.innerText = 'My Tasks'
+document.getElementById('task-title').innerHTML = '<span style="color:red">Task List</span>'
 
 
-let scripts = document.scripts;
-let scriptsArr = Array.from(scripts);
-scriptsArr.forEach(function(script){
-    console.log(script);
-});
 
-console.log(val);
+// document.querySelector()
+console.log(document.querySelector('#tasks-title'));
+console.log(document.querySelector('.card-title'));
+console.log(document.querySelector('h5'));
 
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(3)').style.color = 'green';
+document.querySelector('li:nth-child(4)').textContent = 'Hello World';
+document.querySelector('li:nth-child(odd)').style.background = '#ccc';
+document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
