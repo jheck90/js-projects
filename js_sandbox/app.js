5943364aeb61ecac88b87dc7718e90cf741d5833
John <casillaj@oregonstate.edu>
2018-10-23 13:44:47 -0600

    Adding multiple element stuff, js

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,36 +1,56 @@
-// document.getElementById()
+// document.getElementsByClassName()
 
-console.log(document.getElementById('task-title'));
+// const items = document.getElementsByClassName('collection-item');
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = 'red';
+// items[3].textContent = 'Hello';
 
-// Get things from the element
-console.log(document.getElementById('task-title').id);
-console.log(document.getElementById('task-title').className);
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// console.log(listItems);
 
-const taskTitle = document.getElementById('task-title');
+// document.getElementsByTagName
+// let lis = document.getElementsByTagName('li');
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = 'red';
 
-// Change Styling
-taskTitle.style.background = '#333';
-document.getElementById('task-title').style.color = '#fff';
-document.getElementById('task-title').style.padding = '5px';
-// document.getElementById('task-title').style.display = 'none'; // makes disappear
-
-// Change Content
-taskTitle.textContent = 'Tasks List'
-taskTitle.innerText = 'My Tasks'
-document.getElementById('task-title').innerHTML = '<span style="color:red">Task List</span>'
+// // console.log(items[0]);
+// // items[0].style.color = 'red';
+// // items[3].textContent = 'Hello';
 
 
+// // Convert HTML colelction into array
+// lis = Array.from(lis);
 
-// document.querySelector()
-console.log(document.querySelector('#tasks-title'));
-console.log(document.querySelector('.card-title'));
-console.log(document.querySelector('h5'));
+// lis.reverse();
 
-document.querySelector('li').style.color = 'red';
-document.querySelector('ul li').style.color = 'blue';
+// // index numbers things
+// lis.forEach(function(li, index){
+//     console.log(li.className);
+//     li.textContent = `${index}: Hello`;
+// });
+// console.log(lis);
 
-document.querySelector('li:last-child').style.color = 'red';
-document.querySelector('li:nth-child(3)').style.color = 'green';
-document.querySelector('li:nth-child(4)').textContent = 'Hello World';
-document.querySelector('li:nth-child(odd)').style.background = '#ccc';
-document.querySelector('li:nth-child(even)').style.background = '#f4f4f4';
+// document.querySelectorAll
+const items = document.querySelectorAll('ul.collection li.collection-item');
+
+// didn't have to convert to array first because it's a node list
+items.forEach(function(item, index){
+         console.log(item);
+         item.textContent = `${index}: Hello`;
+     });
+
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)');
+
+// two different ways to loop through collections
+liOdd.forEach(function(li, index){
+    li.style.background = '#ccc'
+});
+
+for(let i = 0; i < liEven.length; i++){
+    liEven[i].style.background = '#f4f4f4'
+}
+
+console.log(items);
