78d78fa1ed20a4870dfde1d4b3dc1735cefc091e
John <casillaj@oregonstate.edu>
2018-10-24 16:29:00 -0600

    some traversing js stuff

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,56 +1,38 @@
-// document.getElementsByClassName()
+let val;
 
-// const items = document.getElementsByClassName('collection-item');
-// console.log(items);
-// console.log(items[0]);
-// items[0].style.color = 'red';
-// items[3].textContent = 'Hello';
-
-// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
-// console.log(listItems);
-
-// document.getElementsByTagName
-// let lis = document.getElementsByTagName('li');
-// console.log(lis);
-// console.log(lis[0]);
-// lis[0].style.color = 'red';
-
-// // console.log(items[0]);
-// // items[0].style.color = 'red';
-// // items[3].textContent = 'Hello';
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
 
 
-// // Convert HTML colelction into array
-// lis = Array.from(lis);
 
-// lis.reverse();
 
-// // index numbers things
-// lis.forEach(function(li, index){
-//     console.log(li.className);
-//     li.textContent = `${index}: Hello`;
-// });
-// console.log(lis);
+val = listItem;
+val = list;
 
-// document.querySelectorAll
-const items = document.querySelectorAll('ul.collection li.collection-item');
+// Get child nodes
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[3].nodeType;
 
-// didn't have to convert to array first because it's a node list
-items.forEach(function(item, index){
-         console.log(item);
-         item.textContent = `${index}: Hello`;
-     });
+// 1 - Element
+// 2 - Attribute
+// 3 - Text Node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
 
-const liOdd = document.querySelectorAll('li:nth-child(odd)');
-const liEven = document.querySelectorAll('li:nth-child(even)');
 
-// two different ways to loop through collections
-liOdd.forEach(function(li, index){
-    li.style.background = '#ccc'
-});
 
-for(let i = 0; i < liEven.length; i++){
-    liEven[i].style.background = '#f4f4f4'
-}
+// Get Children element nodes
+val = list.children;
+val = list.children[0];
+list.children[0].textContent = 'Hello';
 
-console.log(items);
+// Children of Children
+val = list.children[3].children;
+
+val = list.firstChild;
+val = list.firstElementChild;
+
+console.log(val);
